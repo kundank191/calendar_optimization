418589424662fefaabdfa54f007b3b9746574c87
Kundan <kundank191@gmail.com>
2023-02-12 16:36:24 +0000

    render hello world template

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-// create a flask app that renders static/index.html
 from flask import Flask, render_template
 app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('hello_world.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
