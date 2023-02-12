752b7de5a4c45fde4230e71180e7a54fd81ee24b
Kundan <kundank191@gmail.com>
2023-02-12 17:57:10 +0000

    add constants and finalize project

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, send_from_directory
+import constants
 from app.calendar_api import add_alarm_to_calender_events
 
-app = Flask(__name__, static_folder='static', static_url_path='', template_folder='templates')
+app = Flask(__name__, static_folder='static',
+            static_url_path='', template_folder='templates')
+
 
 @app.route('/')
 def index():
@@ -11,18 +14,29 @@ def index():
 
     return render_template('index.html')
 
+
 @app.route('/update_calendar', methods=['POST'])
 def update_calendar():
     """
     This function will update the calendar events with the alarm
     """
     file_name = request.files['file']
+    file_name.save(constants.LOCATION_TO_SAVE_FILES + file_name.filename)
 
-    print(file_name)
+    # add alarm to the calendar events
+    add_alarm_to_calender_events(
+        constants.LOCATION_TO_SAVE_FILES + file_name.filename)
 
-    send_from_directory('./logs/',file_name, as_attachment=True)
+    # return the file to the user
+    return send_from_directory(constants.LOCATION_TO_SAVE_FILES, file_name.filename, as_attachment=True)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+    function will render the 404 page
+    """
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
