0842e2266936958d20003b8c7f23ef0d6e538535
Kundan <kundank191@gmail.com>
2023-02-12 16:59:39 +0000

    add gunicorn conf file and update git ignore

---
+++ gunicorn.conf.py
@@ -0,0 +1,16 @@
+import logging
+import multiprocessing
+import os
+from logging.handlers import TimedRotatingFileHandler
+
+bind = ":8081"
+timeout = 5
+workers = multiprocessing.cpu_count()
+threads = 2
+errorlog = os.path.dirname(__file__) + "/logs/errorlog.log"
+accesslog = os.path.dirname(__file__) + "/logs/accesslog.log"
+errorlog_handler = TimedRotatingFileHandler(errorlog, when='D', interval=1, backupCount=7)
+
+logging.basicConfig(handlers=[errorlog_handler])
+logging.getLogger().setLevel(logging.INFO)
+capture_output = True
